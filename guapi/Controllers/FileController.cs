f5c564882be83c474d969754e6600957aea232e5
216_100007431271_danceruler <216_100007431271_danceruler@git.cloud.tencent.com>
2019-05-11 13:58:12 +0800

    20190511

--- guapi/Controllers/FileController.cs
+++ guapi/Controllers/FileController.cs
@@ -17,35 +17,43 @@ namespace guapi.Controllers
         private guyunhaoEntities db = new guyunhaoEntities();
         private string serverPath = "http://www.mzhdemo.xyz:2001/UploadFile/";
         [HttpPost]
-        public string PostFile()
+        public int PostFile()
         {
             //HttpResponseMessage result = null;
-            string result = "";
-            var httpRequest = HttpContext.Current.Request;
-            var userid = int.Parse(httpRequest.Form["userid"].ToString());
-            if (httpRequest.Files.Count > 0)
+            int result = 0;
+            try
             {
-                var postedFile = httpRequest.Files[0];
-                var filename = postedFile.FileName;
-                var path = HttpContext.Current.Server.MapPath("~/UploadFile/");
-                var nowtime = DateTime.Now;
-                var time = nowtime.ToString("yyyyMMddHHmmss");
-                var filePath = path + userid + "-" + time + "-" + filename;
-                //var filePath = path + userid+","+DateTime.Now.ToString("yyyyMMddHH:mm:ss")+","+filename;
-                postedFile.SaveAs(filePath);
+                var httpRequest = HttpContext.Current.Request;
+                var userid = int.Parse(httpRequest.Form["userid"].ToString());
+                if (httpRequest.Files.Count > 0)
+                {
+                    var postedFile = httpRequest.Files[0];
+                    var filename = postedFile.FileName;
+                    var path = HttpContext.Current.Server.MapPath("~/UploadFile/");
+                    var nowtime = DateTime.Now;
+                    var time = nowtime.ToString("yyyyMMddHHmmss");
+                    var filePath = path + userid + "-" + time + "-" + filename;
+                    //var filePath = path + userid+","+DateTime.Now.ToString("yyyyMMddHH:mm:ss")+","+filename;
+                    postedFile.SaveAs(filePath);
 
-                var user = db.user.SingleOrDefault(t => t.id == userid);
-                uploadfiles uploadfiles = new uploadfiles();
-                uploadfiles.name = filename;
-                uploadfiles.date = nowtime;
-                uploadfiles.userid = userid;
-                uploadfiles.url = filePath.Replace(path, serverPath);
-                uploadfiles.level = user.level;
-                db.uploadfiles.Add(uploadfiles);
-                db.SaveChanges();
-                result = uploadfiles.url;
+                    var user = db.user.SingleOrDefault(t => t.id == userid);
+                    uploadfiles uploadfiles = new uploadfiles();
+                    uploadfiles.name = filename;
+                    uploadfiles.date = nowtime;
+                    uploadfiles.userid = userid;
+                    uploadfiles.url = filePath.Replace(path, serverPath);
+                    uploadfiles.level = user.level;
+                    db.uploadfiles.Add(uploadfiles);
+                    db.SaveChanges();
+                    result = 1;
+                }
+                return result;
             }
-            return result;
+            catch (Exception)
+            {
+                return result;
+            }
+            
         }
 
 
@@ -55,7 +63,7 @@ namespace guapi.Controllers
             List<uploadfiles> uploadfiles = new List<uploadfiles>();
             var user = db.user.SingleOrDefault(t => t.id == userid);
             if (user == null) return uploadfiles;
-            uploadfiles = db.uploadfiles.Where(t => t.level <= user.level).ToList();
+            uploadfiles = db.uploadfiles.Where(t => t.level <= user.level).OrderByDescending(t => t.date).ToList();
             return uploadfiles;
         } 
         
